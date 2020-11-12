c74013384b7b47cf76ea2976606bab20cd9b60a1
Andrew-Hany <andrewhany@aucegypt.edu>
2020-11-12 05:40:41 +0200

    web scrapying

--- MS2 2/DUMB_database_data/Students.sql
+++
@@ -1,5 +0,0 @@
-/*
--- Query: SELECT * FROM catelog.Students
--- Date: 2020-11-11 20:15
-*/
-INSERT INTO `` (`st_name`,`ID`,`GPA`) VALUES ('Andrew',900123456,4.0);
--- MS2 2/DUMB_database_data/StudentsMajor.sql
+++
@@ -1,5 +0,0 @@
-/*
--- Query: SELECT * FROM catelog.StudentsMajor
--- Date: 2020-11-11 20:16
-*/
-INSERT INTO `` (`ID`,`major_code`) VALUES (900123456,'CSCE');
